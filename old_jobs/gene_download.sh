492a41802f1ffdb1b41810fb73ad2df2da8aea29
Dylan Peterson <dylan.peterson95@gmail.com>
2021-06-02 12:17:18 -0700

    updating .sh

--- old_jobs/gene_download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#
-# Loop through chromosomes
-#
-for CHR in {1..26}
-do
-  ./gfetch 22418 -c$CHR -a.ukbkey
-done
--- old_jobs/rename_files.sh
+++
@@ -1,7 +0,0 @@
-# load modules
-for CHR in {1..26}; do
-# for f in *v2.bed; do
-    echo snp_$CHR
-    #mv -- "$f" "${f%.html}.php"
-done
-
